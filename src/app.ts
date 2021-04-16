8eb01a68cb1c38293b94d2442e56b6e66f000ea5
Tecardo1 <tecardo@gmail.com>
2021-04-16 21:34:34 +0200

    grammy test with i18n

--- src/app.ts
+++ src/app.ts
@@ -15,13 +15,6 @@ export interface MyContext extends GrammyContext {
     match: RegExpExecArray | undefined;
 }
 
-const token = process.env.BOT_TOKEN
-// Create an instance of the `Bot` class and pass your bot token to it.
-// @ts-ignore
-const bot =  new Bot<MyContext>(token) // <-- put your bot token between the ''
-
-// You can now register listeners for on your bot object `bot`.
-// grammY will call the listeners when users send messages to your bot.
 //i18n Options
 export const i18n = new I18n({
     directory: path.resolve(path.join(__dirname, '../'), 'locales'),
@@ -33,6 +26,16 @@ export const i18n = new I18n({
         uppercase: (value: string) => value.toUpperCase()
     }
 })
+
+const token = process.env.BOT_TOKEN
+// Create an instance of the `Bot` class and pass your bot token to it.
+// @ts-ignore
+const bot =  new Bot<MyContext>(token) // <-- put your bot token between the ''
+bot.use(i18n.middleware())
+
+// You can now register listeners for on your bot object `bot`.
+// grammY will call the listeners when users send messages to your bot.
+
 // React to /start command
 bot.command('start', (ctx) => ctx.reply(ctx.i18n.t('welcome')))
 // Handle other messages
@@ -40,6 +43,6 @@ bot.on('message', (ctx) => ctx.reply(ctx.i18n.t('message')))
 
 // Now that you specified how to handle messages, you can start your bot.
 // This will connect to the Telegram servers and wait for messages.
-
+bot.catch(e => (console.log(e)))
 // Start your bot
 bot.start().then(r => console.log(r)).catch(e => console.log(e))
