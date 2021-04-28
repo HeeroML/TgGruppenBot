e8676b37dbab75333fb095dca3108d67bf8027bf
Tecardo1 <tecardo@gmail.com>
2021-04-28 21:24:02 +0200

    added some extra comments and command for giving out all session data /session

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import {Bot, session, webhookCallback} from 'grammy'
+import {Bot, session} from 'grammy'
 import {i18n, token} from './config'
 import {MyContext, SessionData} from './types';
 import {backToMain, mainMenuInlineKeyboard} from './menu/mainMenu';
@@ -33,22 +33,36 @@ bot.command('start',
     }
 )
 
+//start command
+bot.command('session',
+    async (ctx) => {
+        const mainMenu = await mainMenuInlineKeyboard(ctx) //Passing ctx to the function, so that it can use ctx.
+        try {
+            await ctx.reply(ctx.i18n.t('session') + ctx.session, {
+                reply_markup: mainMenu
+            })
+        } catch (e) {
+            console.log("Something stupid happened... " + e)
+        }
+    }
+)
+
 //Invest Main Amount
 bot.on("message:text", (ctx) => { // with message:text we filter for all messages of type text, no photos and co.
         const text: string = ctx.msg.text;
         ctx.reply("Echo: " + text) //Just echoing every text back
 });
 
-bot.callbackQuery("optionMenu", async (ctx) => {
+bot.callbackQuery("optionMenu", async (ctx) => { //Going into Option Menu
     await optionsMenu(ctx)
 });
 
-bot.callbackQuery("backHome", async (ctx) => {
-    await backToMain(ctx)
+bot.callbackQuery("backHome", async (ctx) => { //Going back to Home Menu
+    await backToMain(ctx) //This function replies the main menu back
 });
 
 bot.callbackQuery("setLangDE", async (ctx) => {
-    ctx.i18n.locale('de')
+    ctx.i18n.locale('de') //We set the language of our bot with i18n.locale(). This is saved in Session
     await backToMain(ctx)
 });
 
@@ -59,12 +73,12 @@ bot.callbackQuery("setLangEN", async (ctx) => {
 
 bot.on("callback_query:data", async (ctx, next) => { //catching all unknown callback data. This shouldn't happen and is a mistake by you
     console.log("Unknown button event with payload", ctx.callbackQuery.data);
-    await ctx.answerCallbackQuery(); // remove loading animation for callbackquerys
+    await ctx.answerCallbackQuery(); // remove loading animation for callbackquerys, Always answer CallbackQuerys
     return next
 });
 
 bot.catch(error => {
-    console.log('Oh no, we catched a error! ', error)
+    console.log('Oh no, we caught a error! ', error)
 })
 
 
