7eb5d9366c66cd5c44d3e5123b9a5528ad1f4305
Tecardo1 <tecardo@gmail.com>
2021-04-29 20:50:17 +0200

    added some extra comments and command for giving out all session data /session

--- src/config.ts
+++ src/config.ts
@@ -8,12 +8,25 @@ dotenv.config();
 //export const PORT = process.env.PORT;  //Port of your webserver, Important for webhooks
 export const token: string  = process.env.BOT_TOKEN ?? "Ahm, this can't be not defined. Also i'm not inserting a default token here ;)"; //Token of your bot
 
+
+
 //i18n Options
+/*
+Both .json and .yaml is supported
+Example directory structure:
+├── locales
+│   ├── en.yaml
+│   ├── en-US.yaml
+│   ├── it.json
+│   └── ru.yaml
+└── src
+    ├── app.ts
+*/
+
 export const i18n = new I18n({
     directory: path.resolve(path.join(__dirname, "../"), "locales"),
-    defaultLanguage: "en",
-    sessionName: "session",
-    useSession: true,
+    defaultLanguage: "en", //The default language used if there is no value supplied to i18n.t()
+    useSession: true, //We need this for switching
     templateData: {
         pluralize,
         uppercase: (value: string) => value.toUpperCase()
